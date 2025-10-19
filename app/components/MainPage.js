ec2d7708e9b1858332ed426edbb8ad5ba6152c2d
Bani <bbaltawalbeh23@cit.just.edu.jo>
2025-10-19 23:02:22 +0300

    better coundown display

--- app/components/MainPage.js
+++ app/components/MainPage.js
@@ -88,9 +88,8 @@ export default function MainPage() {
 
       const hours = Math.floor(diff / (1000 * 60 * 60));
       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-      return `${hours}h ${minutes}m ${seconds}s`;
+      return `${hours}h:${minutes}m`;
     };
 
     // Initial calculation
